edb0bee112e5d0c026c58b180623460a9dd48a70
Keval2411 <solankikeval43@gmail.com>
2024-06-24 07:29:42 +0100

    project

---
+++ counter_num/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+  const addvalue = () => {
+    if (count < 20){
+    setCount(count + 1)
+    }
+  }
+
+  const removevalue = () => {
+    
+    if (count > 0) {
+      setCount(count - 1);
+    }
+    
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={addvalue}>
+          Add count {count}
+        </button>
+        <button onClick={removevalue}>
+          Remove count {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
