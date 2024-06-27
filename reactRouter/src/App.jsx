fa67e67459186a35135d2e454c3bbcadc52bb8d7
Keval2411 <solankikeval43@gmail.com>
2024-06-27 11:00:54 +0100

    awesome code

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header/Header.jsx'
+import Footer from './Components/Footer/Footer.jsx'
+import Home from './Components/Home/Home.jsx'
+import 'tailwindcss/tailwind.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      
+    </>
+  )
+}
+
+export default App
